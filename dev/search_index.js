8220fc1a9e798b54f8e9e3c09cf50cf4e97e67f3
Documenter.jl <documenter@juliadocs.github.io>
2020-11-24 16:21:58 +0000

    build based on 8a68259

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BikeStats","category":"page"},{"location":"#BikeStats","page":"Home","title":"BikeStats","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BikeStats]","category":"page"}]
+}
